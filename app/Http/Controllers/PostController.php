f23ebecffa26f0d78a9a4266049e0b0b16d238ae
hamagishi aiki <hama80ha@gmail.com>
2024-02-07 00:42:57 +0900

    投稿編集画面

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -12,24 +12,29 @@ class PostController extends Controller
     {
        return view('posts.index')->with(['posts' => $post->getPaginateByLimit(5)]);
     }
+    
     public function show(Post $post)
     {
         return view('posts.show')->with(['post' => $post]);
     }
+    
     public function create()
     {
         return view('posts.create');
     }
+    
       public function store(Post $post, PostRequest $request)
     {
         $input = $request['post'];
         $post->fill($input)->save();
         return redirect('/posts/' . $post->id);
     }
+    
     public function edit(Post $post)
     {
         return view('posts.edit')->with(['post' => $post]);
     }
+    
     public function update(Post $post, PostRequest $request)
     {
         $input_post = $request['post'];
